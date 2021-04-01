02002fb97fc5a4958bcd7d71142b7edd130c0689
omarkhater <omer_ayman2009@yahoo.com>
2021-04-01 04:22:17 +0200

    Before down sampling

--- visuals.py
+++ visuals.py
@@ -1,3 +1,4 @@
+# Made by : Omar Khater
 ###########################################
 # Suppress matplotlib user warnings
 # Necessary for newer version of matplotlib
@@ -17,7 +18,7 @@ from time import time
 from sklearn.metrics import f1_score, accuracy_score
 
 
-def distribution(data, transformed = False):
+def distribution(data,features, transformed = False):
     """
     Visualization code for displaying skewed distributions of features
     """
@@ -26,7 +27,7 @@ def distribution(data, transformed = False):
     fig = pl.figure(figsize = (20,10));
 
     # Skewed feature plotting
-    for i, feature in enumerate(['BILL_AMT'+ str(i) for i in range(1,7)] + ['PAY_AMT'+ str(i) for i in range(1,7)]):
+    for i, feature in enumerate(features):
         ax = fig.add_subplot(4, 3, i+1)
         ax.hist(data[feature], bins = 25, color = '#00A0A0')
         ax.set_title("'%s' Feature Distribution"%(feature), fontsize = 14)
@@ -60,7 +61,7 @@ def evaluate(results, accuracy, f1):
     """
   
     # Create figure
-    fig, ax = pl.subplots(2, 3, figsize = (11,9))
+    fig, ax = pl.subplots(2, 4, figsize = (15,12))
 
     # Constants
     bar_width = 0.3
@@ -68,31 +69,37 @@ def evaluate(results, accuracy, f1):
     
     # Super loop to plot four panels of data
     for k, learner in enumerate(results.keys()):
-        for j, metric in enumerate(['train_time', 'acc_train', 'f_train', 'pred_time', 'acc_test', 'f_test']):
+        for j, metric in enumerate(['train_time', 'acc_train', 'f_train', 'auc_train', 'pred_time', 'acc_test', 'f_test', 'auc_test']):
             for i in np.arange(3):
                 
                 # Creative plot code
-                ax[j//3, j%3].bar(i+k*bar_width, results[learner][i][metric], width = bar_width, color = colors[k])
-                ax[j//3, j%3].set_xticks([0.45, 1.45, 2.45])
-                ax[j//3, j%3].set_xticklabels(["1%", "10%", "100%"])
-                ax[j//3, j%3].set_xlabel("Training Set Size")
-                ax[j//3, j%3].set_xlim((-0.1, 3.0))
+                ax[j//4, j%4].bar(i+k*bar_width, results[learner][i][metric], width = bar_width, color = colors[k])
+                ax[j//4, j%4].set_xticks([0.45, 1.45, 2.45])
+                ax[j//4, j%4].set_xticklabels(["1%", "10%", "100%"])
+                ax[j//4, j%4].set_xlabel("Training Set Size")
+                ax[j//4, j%4].set_xlim((-0.1, 3.0))
     
     # Add unique y-labels
     ax[0, 0].set_ylabel("Time (in seconds)")
     ax[0, 1].set_ylabel("Accuracy Score")
     ax[0, 2].set_ylabel("F-score")
+    ax[0, 3].set_ylabel("AUC-score")
+    
     ax[1, 0].set_ylabel("Time (in seconds)")
     ax[1, 1].set_ylabel("Accuracy Score")
     ax[1, 2].set_ylabel("F-score")
+    ax[1, 3].set_ylabel("AUC-score")
     
     # Add titles
     ax[0, 0].set_title("Model Training")
     ax[0, 1].set_title("Accuracy Score on Training Subset")
     ax[0, 2].set_title("F-score on Training Subset")
+    ax[0, 3].set_title("AUC-score on Training Subset")
+    
     ax[1, 0].set_title("Model Predicting")
     ax[1, 1].set_title("Accuracy Score on Testing Set")
     ax[1, 2].set_title("F-score on Testing Set")
+    ax[1, 3].set_title("AUC-score on Testing Set")
     
     # Add horizontal lines for naive predictors
     ax[0, 1].axhline(y = accuracy, xmin = -0.1, xmax = 3.0, linewidth = 1, color = 'k', linestyle = 'dashed')
@@ -141,3 +148,20 @@ def feature_plot(importances, X_train, y_train):
     pl.legend(loc = 'upper center')
     pl.tight_layout()
     pl.show()  
+
+def customer_hist(data, customer_idx = 32):
+    """
+    Line plot for billed vs Paid amounts for given customer index
+    """
+    print('Index: {}, Default? {}'.format(customer_idx, data.iloc[customer_idx]['Y']))
+    y1 = data.iloc[customer_idx][['BILL_AMT' + str(i) for i in range(1,7)]]
+    y2 = data.iloc[customer_idx][['PAY_AMT' + str(i) for i in range(1,7)]]
+    x = ['April','May','June','July','August','Septemper']
+    fig,ax = pl.subplots()
+    ax.grid();
+    ax.set_xlabel('Month');
+    ax.set_ylabel('Amount (In thousands)');
+    ax.plot(x,y1 * .001, x,y2 * .001);
+    ax.legend(['Billed', 'Paid']);
+    ax.set_title('Customer History (Billed Vs Paid) for customer idx = {}'.format(customer_idx))
+    pl.show()
